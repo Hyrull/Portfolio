82047c363920900743b924ad133a2a3d99d0e2a5
Hyrul <xavier.leonard@outlook.com>
2024-06-17 17:24:28 +0200

    temp rename to update TS casing

--- src/Main.tsx
+++ src/Main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 // import  { createRoot } from 'react-dom/client'
 import ErrorPage from './pages/ErrorPage/ErrorPage'
-import Home from './pages/Home/Home'
+import Home from './pages/Home/HomeTemp'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
