a7065650657b5cdd48f6078161c644e60cac1d03
Hyrul <xavier.leonard@outlook.com>
2024-06-17 17:24:50 +0200

    back to correct name/casing

--- src/Main.tsx
+++ src/Main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 // import  { createRoot } from 'react-dom/client'
 import ErrorPage from './pages/ErrorPage/ErrorPage'
-import Home from './pages/Home/HomeTemp'
+import Home from './pages/Home/Home'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
