9a05ad2eeae16d1fda5f18571ebd8aa1f2958135
Hyrul <xavier.leonard@outlook.com>
2024-06-18 03:35:48 +0200

    made font global

--- src/Main.tsx
+++ src/Main.tsx
@@ -1,20 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { Routes, Route, BrowserRouter } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 // import  { createRoot } from 'react-dom/client'
 import ErrorPage from './pages/ErrorPage/ErrorPage'
 import Home from './pages/Home/Home'
 import Header from './components/Header/Header'
-
+import 'primereact/resources/themes/bootstrap4-dark-blue/theme.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <Router>
       <Header />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='*' element={<ErrorPage />} />
       </Routes>
-    </BrowserRouter>
+    </Router>
   </React.StrictMode>,
 )
