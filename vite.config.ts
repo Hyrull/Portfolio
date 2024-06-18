cadd5a674eaba6192d76f95fcc6a3bf44794f50f
Hyrul <xavier.leonard@outlook.com>
2024-06-18 05:13:12 +0200

    updated config

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import { fileURLToPath } from 'url';
+import path from 'path';
 
-// https://vitejs.dev/config/
+// Convert import.meta.url to a file path
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 export default defineConfig(({ command }) => {
-  const config = {
+  return {
     plugins: [react()],
-    base: '/',
-  }
-
-  if (command !== 'serve') {
-    config.base = '/Portfolio/'
-  }
-
-  return config
-})
+    base: command === 'serve' ? '/' : '/',
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src'),
+      },
+    },
+    build: {
+      outDir: 'dist',
+      rollupOptions: {
+        input: path.resolve(__dirname, 'index.html'),
+      },
+    },
+  };
+});
