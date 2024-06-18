ad2139ca45d2c27c331374d13a42f32d769e8381
Hyrul <xavier.leonard@outlook.com>
2024-06-18 06:22:43 +0200

    updated config

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,27 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import { fileURLToPath } from 'url';
+import path from 'path';
 
-export default defineConfig({
-  plugins: [react()],
+// Convert import.meta.url to a file path
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default defineConfig(({ command }) => {
+  return {
+    plugins: [react()],
+    base: command === 'serve' ? '/' : '/',
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src'),
+        "$Montserrat": path.resolve('./src/assets/fonts/montserrat-v26-latin-regular.woff2')
+      },
+    },
+    build: {
+      outDir: 'dist',
+      rollupOptions: {
+        input: path.resolve(__dirname, '/index.html'),
+      },
+    },
+  };
 });
