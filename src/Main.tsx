0a3748087d9d486fe078543a45f2e5a68b0aca69
Hyrul <xavier.leonard@outlook.com>
2024-06-18 03:22:01 +0200

    updating for gh pages

--- src/Main.tsx
+++ src/Main.tsx
@@ -5,11 +5,11 @@ import { Routes, Route, BrowserRouter } from 'react-router-dom'
 import ErrorPage from './pages/ErrorPage/ErrorPage'
 import Home from './pages/Home/Home'
 import Header from './components/Header/Header'
-import 'primereact/resources/themes/bootstrap4-dark-blue/theme.css';
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter basename={import.meta.env.DEV ? '/' : '/react-vite-gh-pages/'}>
+    <BrowserRouter>
       <Header />
       <Routes>
         <Route path='/' element={<Home />} />
