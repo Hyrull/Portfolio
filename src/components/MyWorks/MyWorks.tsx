762430eb28c07921904ed1691304c3fdd3ffeb85
Hyrul <xavier.leonard@outlook.com>
2024-06-19 01:42:58 +0200

    myworks is now dynamically rendered (datamapping)

--- src/components/MyWorks/MyWorks.tsx
+++ src/components/MyWorks/MyWorks.tsx
@@ -1,14 +1,12 @@
 import './MyWorks.scss'
 import { Button } from 'primereact/button'
 import { Card } from 'primereact/card'
-import { ExternalLink, GithubLinks } from '../../types/types'
+import { Link } from 'react-router-dom'
+import { GithubLinks } from '../../types/types'
+import projectData from './../../assets/data/projects.json'
 
 const githubRepo = ({ repo } : GithubLinks) => {
   window.open(`https://github.com/hyrull/${repo}`, '_blank')
-}
-
-const externalLink = ({ link } : ExternalLink) => {
-  window.open(`${link}`, '_blank')
 }
 
 function MyWorks() {
@@ -18,110 +16,19 @@ function MyWorks() {
       <h1>The projects I've worked on:</h1>
       <h2>And thus, the skills I've acquired</h2>
       <div className='all-cards'>
-        <Card 
-        title='Booki'
-        subTitle="HTML & CSS">
-          <ul>
-            <li>Initiation à l'HTML & CSS</li>
-            <li>Savoir reproduire une maquette à l'identique</li>
-            <li>Travail de la responsiveness</li>
-          </ul>
-          <div className='card-buttons'>
-            <Button label="View on GitHub"
-            onClick={() => githubRepo({ repo: 'OC-P2' })} />
-          </div>
-        </Card>
-        <Card 
-        title='Sophie Bluel'
-        subTitle='JavaScript'>
-          <ul>
-            <li>Récupérer des données frontend</li>
-            <li>Gestion du DOM avec JavaScript</li>
-            <li>Gestion des évènements utilisateurs</li>
-          </ul>
-          <div className='card-buttons'>
-            <Button label="View on GitHub" 
-            onClick={() => githubRepo({ repo: 'OC-P3' })}/>
-          </div>
-        </Card>
-        <Card 
-        title='Nina Carducci'
-        subTitle='SEO Optimizations'>
-          <ul>
-            <li>Optimiser les performances d'un site</li>
-            <li>Optimiser la SEO & accessibilité</li>
-            <li>Rédiger un cahier de recette</li>
-          </ul>
-          <div className='card-buttons'>
-            <Button label="View on GitHub" 
-            onClick={() => githubRepo({ repo: 'OC-P5' })}/>
-          </div>
-        </Card>
-        <Card 
-        title='Kasa'
-        subTitle='React.js'>
-          <ul>
-            <li>Initialiser un site avec Vite</li>
-            <li>Création d'une Single Page Application</li>
-            <li>Utilisation des pages & components de React</li>
-          </ul>
-          <div className='card-buttons'>
-            <Button label="View on GitHub" 
-            onClick={() => githubRepo({ repo: 'OC-P6' })}/>
-          </div>
-        </Card>
-        <Card 
-        title='Mon Vieux Grimoire'
-        subTitle='Node.js & Express.js'>
-          <ul>
-            <li>Green coding & database sécurisée</li>
-            <li>Système CRUD avec authentification</li>
-            <li>Implémenter un modèle logique optimisé</li>
-          </ul>
-          <div className='card-buttons'>
-            <Button label="View on GitHub" 
-            onClick={() => githubRepo({ repo: 'OC-P7' })}/>
-          </div>
-        </Card>
-        <Card 
-        title='Mon Portfolio'
-        subTitle='Vous y êtes.'>
-          <ul>
-            <li>Déploiement d'un site avec GitHub Pages</li>
-            <li>Design d'un site sous React & React Router</li>
-            <li>Rédaction d'un contenu propre et séduisant</li>
-          </ul>
-          <div className='card-buttons'>
-            <Button label="View on GitHub" 
-            onClick={() => githubRepo({ repo: 'hyrull.github.io' })}/>
-          </div>
-        </Card>
-        <Card 
-        title='Floriant Dupont'
-        subTitle='Site web en ligne'>
-          <ul>
-            <li>Site web HTML&CSS seulement</li>
-            <li>Réalisé sur place avec suivi client</li>
-            <li>Code simple sur demande du client</li>
-          </ul>
-          <div className='card-buttons'>
-            <Button label="View live" 
-            onClick={() => externalLink({ link: 'http://etsdupont.fr/' })}/>
-          </div>
-        </Card>
-        <Card 
-        title='Sarge'
-        subTitle='Discord.js'>
-          <ul>
-            <li>Premier projet de backend</li>
-            <li>Gestion d'un environnement sécurisé</li>
-            <li>Programmation JavaScript continue</li>
-          </ul>
-          <div className='card-buttons'>
-            <Button label="View on GitHub" 
-            onClick={() => githubRepo({ repo: 'Sarge' })}/>
-          </div>
-        </Card>
+        {projectData.map(project => (
+          <Card key={project.id} title={project.title} subTitle={project.subtitle}>
+            <ul>
+              {project.details.map((detail, index) => (
+                <li key={index}>{detail}</li>
+              ))}
+            </ul>
+            <div className='card-buttons'>
+              {/* <Link to={`/projects/${project.id}`}>Learn More</Link> */}
+              <Button label='View on GitHub' onClick={() => githubRepo({ repo: project.id })} />
+            </div>
+          </Card>
+        ))}
         <Card 
         title='Et ensuite ?'
         subTitle='On continue'>
