8f9e5941bec2854383ac20e997fabc37cd92d18c
Hyrul <xavier.leonard@outlook.com>
2024-06-19 21:35:45 +0200

    semantic & accessibility updates

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -5,7 +5,7 @@ import linkedinLogo from './../../assets/img/linkedin.svg'
 function Footer() {
   return (
     <footer>
-      <p>Site créé par Xavier Léonard</p>
+      <h2>Site créé par Xavier Léonard</h2>
       <div className='networks'>
         <a href='https://github.com/hyrull'><img src={githubLogo} alt='Logo de GitHub'/></a>
         <a href='https://www.linkedin.com/in/xavier-leonard-35a6ab19a/'><img src={linkedinLogo} alt='Logo de LinkedIn'/></a>
--- src/components/HomeContent/HomeContent.tsx
+++ src/components/HomeContent/HomeContent.tsx
@@ -8,10 +8,10 @@ function HomeContent() {
       <img src={background} alt='Image de fond'/>
       <div className='main-container'>
         <div className='home-content-text'>
-          <h3>Bienvenue.</h3>
+          <h2>Bienvenue.</h2>
           <h1>Je suis un développeur web junior;</h1><br/>
           <h1>en recherche d'emploi en Nouvelle-Aquitaine.</h1>
-          <h3>Scrollez pour me découvrir.</h3>
+          <h2>Scrollez pour me découvrir.</h2>
         </div>
         <div className='home-content-links'>
           <Link to='my-skills' smooth={true} duration={500}>Compétences</Link>
