8446c60aae9478e7666b040e82b276b57f101c07
Hyrul <xavier.leonard@outlook.com>
2024-06-19 04:51:17 +0200

    updated nav

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,16 +1,25 @@
 import './Header.scss'
 import { Link } from 'react-scroll'
-import { NavLink } from 'react-router-dom'
+import { NavLink, useLocation } from 'react-router-dom'
 
 function Header() {
+  const location = useLocation();
+  const isHomepage = location.pathname === '/';
+
   return (
     <header>
     <NavLink to='/'><h1>Hyrul</h1></NavLink>
+    {isHomepage ? (
     <nav>
         <Link to='my-skills' smooth={true} duration={500}>My skills</Link>
         <Link to='my-works' smooth={true} duration={500}>My works</Link>
         <Link to='about-me' smooth={true} duration={500}>About me</Link>
     </nav>
+    ) : (
+    <nav>
+        <NavLink to='/'>Back to homepage</NavLink>
+    </nav>
+    )}
     </header>
   )
 }
