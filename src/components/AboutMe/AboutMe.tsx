9e3e09415f1ddec3642659e6270ee10df591f445
Hyrul <xavier.leonard@outlook.com>
2024-06-19 21:48:55 +0200

    improved wording

--- src/components/AboutMe/AboutMe.tsx
+++ src/components/AboutMe/AboutMe.tsx
@@ -15,7 +15,7 @@ function AboutMe() {
             <p>Doté d'une approche optimisée, et passionné, et d'un oeil soucieux du détail, je suis un développeur web junior.
             Autodidacte, je sais comment relever un défi, accomplir les objectifs et amener un projet à terme.
             Mes expériences passées - de la logistique en usine à l'ingénierie du son - m'ont permis de connaître mes besoin professionnels.
-            Je suis donc en recherche d'un emploi me permettant d'assouvir mon désir de compétences et de savoir dans le développement web.</p>
+            Je suis donc en recherche d'un emploi me permettant de m'enrechir en compétences et de savoir dans le développement web.</p>
             <div className='contact'>
               <a href='mailto:placeholder@outlook.com'>Envoyez-moi un mail</a>
               <a href='https://wow.zamimg.com/uploads/screenshots/normal/1149837-sergent.jpg'>Téléchargez mon CV</a>
