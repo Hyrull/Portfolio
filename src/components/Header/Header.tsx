ae289767ebf35f810465be3a9aa75946e89c4709
Hyrul <xavier.leonard@outlook.com>
2024-06-19 17:41:45 +0200

    updated header & moved variables to dedicated file

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -8,18 +8,20 @@ function Header() {
 
   return (
     <header>
-    <NavLink to='/'><h1>Hyrul</h1></NavLink>
-    {isHomepage ? (
-    <nav>
-        <Link to='my-skills' smooth={true} duration={500}>My skills</Link>
-        <Link to='my-works' smooth={true} duration={500}>My works</Link>
-        <Link to='about-me' smooth={true} duration={500}>About me</Link>
-    </nav>
-    ) : (
-    <nav>
-        <NavLink to='/'>Back to homepage</NavLink>
-    </nav>
-    )}
+      <div className='main-container'>
+        <NavLink to='/'><h1>Hyrul</h1></NavLink>
+        {isHomepage ? (
+          <nav>
+            <Link to='my-skills' smooth={true} duration={500}>My skills</Link>
+            <Link to='my-works' smooth={true} duration={500}>My works</Link>
+            <Link to='about-me' smooth={true} duration={500}>About me</Link>
+        </nav>
+        ) : (
+          <nav>
+            <NavLink to='/'>Back to homepage</NavLink>
+        </nav>
+        )}
+    </div>
     </header>
   )
 }
