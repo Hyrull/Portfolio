3b670c7f2545c83de9b1420e57144f266ebaac53
Hyrul <xavier.leonard@outlook.com>
2024-06-20 18:42:24 +0200

    added fadein fx to titles

--- src/components/HomeContent/HomeContent.tsx
+++ src/components/HomeContent/HomeContent.tsx
@@ -8,15 +8,15 @@ function HomeContent() {
       <img src={background} alt='Image de fond'/>
       <div className='main-container'>
         <div className='home-content-text'>
-          <h2>Bienvenue.</h2>
-          <h1>Je suis un développeur web junior;</h1><br/>
-          <h1>en recherche d'emploi en Nouvelle-Aquitaine.</h1>
-          <h2>Scrollez pour me découvrir.</h2>
+          <h2 className='fade-in'>Bienvenue.</h2>
+          <h1 className='fade-in-two'>Je suis un développeur web junior;</h1><br/>
+          <h1 className='fade-in-three'>en recherche d'emploi en Nouvelle-Aquitaine.</h1>
+          <h2 className='fade-in-four'>Scrollez pour me découvrir.</h2>
         </div>
         <div className='home-content-links'>
-          <Link to='my-skills' smooth={true} duration={500}>Compétences</Link>
-          <Link to='my-works' smooth={true} duration={500}>Projets</Link>
-          <Link to='about-me' smooth={true} duration={500}>À propos</Link>
+          <Link to='my-skills' smooth={true} duration={500} className='fade-in-five'>Compétences</Link>
+          <Link to='my-works' smooth={true} duration={500} className='fade-in-six'>Projets</Link>
+          <Link to='about-me' smooth={true} duration={500} className='fade-in-seven'>À propos</Link>
         </div>
       </div>
     </section>
